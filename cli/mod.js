b618bccf374b1bae852b5262192d8910d4a38607
Jan Johannes <jan@ntr.io>
2021-10-28 08:37:41 +0100

    bump version
    Signed-off-by: Jan Johannes <jan@ntr.io>

--- cli/mod.js
+++ cli/mod.js
@@ -27,7 +27,7 @@ import { globToRegExp } from '../deps-deno.js'
 
 // TODO integrate node scripts
 // TODO: sourcemaps worker and svelte, use sourcemaps for watch rebuild dependencies
-export const version = '0.3.1'
+export const version = '0.3.2'
 const denoVersion = '1.14.2'
 let buildName = ''
 let buildColor = ''
