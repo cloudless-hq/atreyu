4ee506af50d421368660a693e69a672d5b5d7489
Jan Johannes <jan@ntr.io>
2021-10-29 11:16:56 +0100

    fix broken version number

--- cli/mod.js
+++ cli/mod.js
@@ -27,7 +27,7 @@ import { globToRegExp } from '../deps-deno.js'
 
 // TODO integrate node scripts
 // TODO: sourcemaps worker and svelte, use sourcemaps for watch rebuild dependencies
-export const version = '0.3.4'
+export const version = '0.3.6'
 // const denoVersion = '1.14.2'
 let buildName = ''
 let buildColor = ''
