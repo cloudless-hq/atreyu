ffaeedc45c7bd433fa523c14295bf54cef3bc8c4
Jan Johannes <jan@ntr.io>
2022-03-16 00:03:55 +0100

    release fix

--- cli/mod.js
+++ cli/mod.js
@@ -28,7 +28,7 @@ import { globToRegExp } from '../deps-deno.ts'
 // TODO integrate node scripts
 // TODO: sourcemaps worker and svelte, use sourcemaps for watch rebuild dependencies
 // TODO: load from tag!
-export const ayuVersion = '0.6.5'
+export const ayuVersion = '0.6.6'
 
 const pinnedVersions = { ipfs: '0.12.0', atreyu: ayuVersion, deno: '1.19.0' }
 
