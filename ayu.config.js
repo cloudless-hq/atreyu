d6e6aed1fdb3c58ffd3aefe16eac15fb92ff4ab0
Jan Johannes <jan@ntr.io>
2023-03-06 13:27:53 +0200

    move dev specific config to secrets file
    Signed-off-by: Jan Johannes <jan@ntr.io>

--- ayu.config.js
+++ ayu.config.js
@@ -1,9 +1,7 @@
 import secrets from './secrets.js'
 
 export default {
-  ELASTIC_URL: 'https://elk.es.europe-west3.gcp.cloud.es.io:9243/logs_dev/_doc/',
   domain: 'atreyu.dev',
-  couchHost: 'https://c3b0b243-4f69-4cb1-9ece-1b0561a67cee-bluemix.cloudant.com',
   kv_namespaces: ['ipfs'],
   ...secrets
 }
